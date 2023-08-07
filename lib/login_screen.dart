06978fe820b235b0b5f4d75d3efd25e6aa75a159
dhruvivaghasiya <dhruvivaghasiya143@gmail.com>
2023-08-07 13:53:31 +0530

    login Screen

---
+++ lib/login_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class Login_screen extends StatefulWidget {
+  const Login_screen({Key? key}) : super(key: key);
+
+  @override
+  State<Login_screen> createState() => _Login_screenState();
+}
+
+class _Login_screenState extends State<Login_screen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
