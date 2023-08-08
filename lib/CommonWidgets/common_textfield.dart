6e03415a20111353c587582c8746767a45845dc1
dhruvivaghasiya <dhruvivaghasiya143@gmail.com>
2023-08-08 11:57:04 +0530

    login Screen

--- lib/CommonWidgets/common_textfield.dart
+++ lib/CommonWidgets/common_textfield.dart
@@ -5,11 +5,11 @@ class CommonTextfield extends StatefulWidget {
   const CommonTextfield({
     Key? key,
     this.hint,
-    this.suffixicon,
+    this.prifixicon,
     this.controller,
   }) : super(key: key);
   final hint;
-  final suffixicon;
+  final prifixicon;
   final controller;
 
   @override
@@ -20,20 +20,23 @@ class _CommonTextfieldState extends State<CommonTextfield> {
   @override
   Widget build(BuildContext context) {
     return TextField(
+      maxLines: 1,
+      textInputAction: TextInputAction.next,
       controller: widget.controller,
       decoration: InputDecoration(
-        contentPadding: EdgeInsets.only(top: 5, left: 10),
-        suffixIcon: widget.suffixicon,
+        contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+        prefixIcon: widget.prifixicon,
         enabledBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(5),
-            borderSide: BorderSide(color: Colors.grey)),
+            borderSide: BorderSide(color: Colors.black)),
         focusedBorder:
             OutlineInputBorder(borderRadius: BorderRadius.circular(5)),
         hintText: widget.hint,
+        hintStyle: TextStyle(fontSize: 15),
         fillColor: Colors.grey.shade200,
         filled: true,
         constraints: BoxConstraints.expand(
-          height: 70.h,
+          height: 80.h,
           width: 388.w,
         ),
       ),
--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -43,38 +43,77 @@ class _LoginScreenState extends State<LoginScreen> {
                           image: AssetImage("assets/images/logo.jpg"))),
                 ),
                 SizedBox(
-                  height: 10.h,
+                  height: 30.h,
                 ),
                 Text(
                   "Login",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
                 ),
-                SizedBox(height: 100.h),
+                SizedBox(height: 80.h),
                 CommonTextfield(
+                  prifixicon: Icon(Icons.email_outlined, color: Colors.black),
                   controller: emailcontroller,
-                  hint: "Email",
+                  hint: "Enter your Email",
                 ),
-                SizedBox(height: 10.h),
-                CommonTextfield(hint: "Password"),
-                SizedBox(height: 5.h),
+                CommonTextfield(
+                  hint: "Enter your Password",
+                  controller: passwordcontroller,
+                  prifixicon:
+                      Icon(Icons.lock_open_outlined, color: Colors.black),
+                ),
+                // SizedBox(height: 5.h),
                 Padding(
                   padding: EdgeInsets.only(left: 220),
                   child: Text(
                     "Forget password?",
-                    style: TextStyle(fontSize: 12, color: Colors.grey.shade600),
+                    style: TextStyle(fontSize: 13, color: Colors.grey.shade600),
                   ),
                 ),
-                SizedBox(height: 20.h),
+                SizedBox(height: 40.h),
                 CommonButton(
                   onpress: () {},
-                  height: 70.h,
+                  height: 60.h,
                   width: 390.w,
                   text: Text(
                     "Login",
                     style: TextStyle(color: Colors.white),
                   ),
                 ),
-                SizedBox(height: 5.h),
+                SizedBox(height: 10.h),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        color: Colors.black54,
+                        // height: 3,
+                        thickness: 2,
+                        indent: 20,
+                        endIndent: 20,
+                      ),
+                    ),
+                    Text("Or sign with"),
+                    Expanded(
+                      child: Divider(
+                        color: Colors.black54,
+                        // height: 3,
+                        thickness: 2,
+                        indent: 20,
+                        endIndent: 20,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20.h),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/googlee.png',
+                      scale: 5,
+                    )
+                  ],
+                ),
+                SizedBox(height: 20.h),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
